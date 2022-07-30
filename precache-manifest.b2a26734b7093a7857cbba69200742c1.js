4fabf6103aa3e9407282ed52148dace2090b8dfe
DESKTOP-C5V1G64\pvngu <pvnguyen@csu.fullerton.edu>
2022-07-30 11:04:18 -0700

    ethswap

---
+++ precache-manifest.b2a26734b7093a7857cbba69200742c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7568b727111872962e68",
+    "url": "/static/css/main.1a18f423.chunk.css"
+  },
+  {
+    "revision": "7568b727111872962e68",
+    "url": "/static/js/main.7568b727.chunk.js"
+  },
+  {
+    "revision": "72024c45fd09927e2642",
+    "url": "/static/css/1.38db66d9.chunk.css"
+  },
+  {
+    "revision": "72024c45fd09927e2642",
+    "url": "/static/js/1.72024c45.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "cce48d61638c259f62942e5bc7eecb5b",
+    "url": "/index.html"
+  }
+];
